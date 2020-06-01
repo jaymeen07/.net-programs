a1ae7ee468050c4a27b27b9268821bac4bf50c57
bhavyarathod <47755136+bhavyarathod@users.noreply.github.com>
2020-06-01 13:12:48 +0530

    Add files via upload

---
+++ Practical6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String number = Console.ReadLine();
+            Console.WriteLine(Double.Parse(number) + "Rupee = " + RuppeToUSDollar(Double.Parse(number)) + "$");
+            Console.WriteLine(Double.Parse(number) + "$ = " + RuppeToUSDollar(Double.Parse(number)) + "Rupee");
+            Console.ReadLine();
+        }
+
+        static double RuppeToUSDollar(double rupee)
+        {
+            return rupee / 71.84;
+        }
+
+        static double USDollarToRupee(double dollar)
+        {
+            return dollar * 71.84;
+        }
+
+
+    }
+}
