e7db227405997712e97c34dc51acce7824689a4f
bhavyarathod <47755136+bhavyarathod@users.noreply.github.com>
2020-06-01 13:17:01 +0530

    Add files via upload

---
+++ Practical5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Practical5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String number = Console.ReadLine();
+            if(number.Length==1)
+            {
+                Console.WriteLine( Digit.onedigit( Int32.Parse(number) ) );
+            }
+            else if(number.Length==2)
+            {
+                Console.WriteLine( Digit.twodigit( Int32.Parse(number) ) );
+            }
+            else if(number.Length==3)
+            {
+                Console.WriteLine( Digit.threedigit( Int32.Parse(number) ) );
+            }
+            else if(number.Length==4)
+            {
+                Console.WriteLine( Digit.fourdigit( Int32.Parse(number) ) );
+            }
+            else if(number.Length==5)
+            {
+                Console.WriteLine(Digit.fivedigit(Int32.Parse(number)));
+            }
+            else if(number.Length==6)
+            {
+                Console.WriteLine(Digit.sixdigit(Int32.Parse(number)));
+            }
+            else
+            {
+                Console.WriteLine("Enter Number Only");
+            }
+            Console.WriteLine("Press Enter For Exit...");
+            Console.ReadLine();
+        }
+    }
+}
