b13618bc39574efcdc91a7c17446b2f1e4026157
bhavyarathod <47755136+bhavyarathod@users.noreply.github.com>
2020-06-01 13:11:36 +0530

    Add files via upload

---
+++ Practical1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Practical1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pattern1(); //This will print Pattern_1
+            Console.WriteLine();
+            pattern2(); //This will print Pattern_2
+            Console.WriteLine();
+            pattern3(); //This will print Pattern_3
+            Console.Read();
+        }
+        static void pattern1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void pattern2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = i; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void pattern3()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
---
+++ Practical2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Practical2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name, country;
+            do {
+                Console.WriteLine("What is your Name: ");
+                name = Console.ReadLine();
+            } while (!Regex.IsMatch(name,"^[a-zA-Z]+$"));
+            do {
+                Console.WriteLine("Where are you from: ");
+                country = Console.ReadLine();
+            } while (!Regex.IsMatch(country, "^[a-zA-Z]+$"));
+            Console.WriteLine("Hello " + name + " from country " + country);
+            Console.ReadLine();
+
+        }
+    }
+}
